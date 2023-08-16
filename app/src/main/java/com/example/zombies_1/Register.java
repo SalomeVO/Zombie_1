d3a376b483feaccad23cab7ae54749231fb180c7
SaloméVO <mvasquezo10@miumg.edu.gt>
2023-08-15 21:02:30 -0600

    funcion de registrarse y de menu funcionando

--- app/src/main/java/com/example/zombies_1/Register.java
+++ app/src/main/java/com/example/zombies_1/Register.java
@@ -3,31 +3,77 @@ package com.example.zombies_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
+//Importacion de clase
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Register extends AppCompatActivity {
+
+    //Declaracion de variables
+
+    private EditText  editTextEmail, editTextPassword, editTextName;
+    private ImageButton buttonRegister;
+    private int startingPoints = 0; // Puntos iniciales
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Relacionamos las variables declaradas con los id de la interfaz
+        editTextEmail = findViewById(R.id.editTextTextEmailAddress4);
+        editTextPassword = findViewById(R.id.editTextTextPassword2);
+        editTextName = findViewById(R.id.editTextText2);
+        buttonRegister = findViewById(R.id.imageButton3);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtener valores ingresados por el usuario
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+                String name = editTextName.getText().toString();
+
+                // Obtener la fecha actual
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String currentDate = sdf.format(new Date());
+
+                // Guardar la información de registro en SharedPreferences
+                SharedPreferences sharedPreferences = getSharedPreferences("UserData", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("email", email);
+                editor.putString("password", password);
+                editor.putString("name", name);
+                editor.putString("registrationDate", currentDate);
+                editor.putInt("points", startingPoints);
+                editor.apply();
+
+                // Construir el mensaje de registro exitoso
+                String registrationInfo = "Fecha: " + currentDate + "\nCorreo: " + email + "\nContraseña: " + password + "\nNombre: " + name + "\nPuntos: " + startingPoints;
+
+                // Mostrar un mensaje de registro exitoso
+                Toast.makeText(Register.this, "Registro exitoso:\n" + registrationInfo, Toast.LENGTH_LONG).show();
+
+                // Redirigir al usuario al menú
+                Intent intent = new Intent(Register.this, menu.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
     //Funcion para el boton de regreso al menu de inicio
     public void regresar_inico(View view){
         Intent intent = null;
         intent = new Intent(this, MainActivity.class);
-
-        if(intent!=null){
-            startActivity(intent);
-        }
-    }
-
-    //Funcion de prueba: en direccionar al menu del usuario ya logeado
-    public void menu_usuario(View view){
-        Intent intent = null;
-        intent = new Intent(this, menu.class);
 
         if(intent!=null){
             startActivity(intent);
--- app/src/main/java/com/example/zombies_1/login.java
+++ app/src/main/java/com/example/zombies_1/login.java
@@ -6,15 +6,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+//Importar clases
+import android.content.SharedPreferences;
+import android.widget.EditText;
+import android.widget.Toast;
+
 public class login extends AppCompatActivity {
+
+    //Declaracion de variables
+    private EditText editTextLoginEmail, editTextLoginPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Relacionar las variables con los elementos de la interfaz
+        editTextLoginEmail = findViewById(R.id.editTextTextEmailAddress);
+        editTextLoginPassword = findViewById(R.id.editTextTextPassword);
     }
 
-    //Funcion para el boton de regreso al menu de inicio
+    // Función para el botón de inicio de sesión
+    public void iniciarSesion(View view) {
+
+        // Obtener valores ingresados por el usuario
+        String loginEmail = editTextLoginEmail.getText().toString();
+        String loginPassword = editTextLoginPassword.getText().toString();
+
+        // Recuperar la información almacenada en SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("UserData", MODE_PRIVATE);
+        String savedEmail = sharedPreferences.getString("email", "");
+        String savedPassword = sharedPreferences.getString("password", "");
+
+        // Verificar la autenticación
+        if (loginEmail.equals(savedEmail) && loginPassword.equals(savedPassword)) {
+            // Inicio de sesión exitoso
+            Toast.makeText(login.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+
+            // Redirigir al usuario a la vista de menú
+            Intent intent = new Intent(login.this, menu.class);
+            startActivity(intent);
+        } else {
+            // Inicio de sesión fallido
+            Toast.makeText(login.this, "Inicio de sesión fallido", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+//Funcion para el boton de regreso al menu de inicio
     public void regresar_inico(View view){
         Intent intent = null;
         intent = new Intent(this, MainActivity.class);
@@ -24,13 +62,4 @@ public class login extends AppCompatActivity {
         }
     }
 
-    //Funcion de prueba: en direccionar al menu del usuario ya logeado
-    public void menu_usuario(View view){
-        Intent intent = null;
-        intent = new Intent(this, menu.class);
-
-        if(intent!=null){
-            startActivity(intent);
-        }
-    }
 }
